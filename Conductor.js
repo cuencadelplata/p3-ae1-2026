91e1a5fb01704b1a58950524f3ebd5588bb339a0
Bautista <bautistasoto918@gmail.com>
2026-08-28 16:05:54 -0300

    Refactor Conductor class to simplify constructor and update getters/setters

--- Conductor.js
+++ Conductor.js
@@ -1,10 +1,8 @@
 class Conductor {
-    constructor(nombre, apellido, ciudad, dni, telefono, tipovehiculo, licenciaId, vehiculoId) {
-        this.nombre = nombre;
-        this.apellido = apellido;
+    constructor(usuarioID,ciudad, tipovehiculo, licenciaId, vehiculoId) {
+        //se trae de la API de M1
+        this.usuarioID = usuarioID;
         this.ciudad = ciudad;
-        this.dni = dni;
-        this.telefono = telefono;
         this.tipovehiculo = tipovehiculo;
 
         //Se trae de la API de licencias y vehiculos
@@ -14,22 +12,16 @@ class Conductor {
     }
 
     // Getters
-    getnombre() { return this._nombre; }
-    getapellido() { return this._apellido; }
+    getusuarioID() { return this._usuarioID; }
     getciudad() { return this._ciudad; }
-    getdni() { return this._dni; }
-    gettelefono() { return this._telefono; }
     gettipovehiculo() { return this._tipovehiculo; }
     getlicenciaId() { return this._licenciaId; }
     getvehiculoId() { return this._vehiculoId; }
     gethabilitado() { return this._habilitado; }
 
     // Setters
-    setnombre(nombre) { this._nombre = nombre; }
-    setapellido(apellido) { this._apellido = apellido; }
+    setusuarioID(usuarioID) { this._usuarioID = usuarioID; }
     setciudad(ciudad) { this._ciudad = ciudad; }
-    setdni(dni) { this._dni = dni; }
-    settelefono(telefono) { this._telefono = telefono; }
     settipovehiculo(tipovehiculo) { this._tipovehiculo = tipovehiculo; }
     setlicenciaId(licenciaId) { this._licenciaId = licenciaId; }
     setvehiculoId(vehiculoId) { this._vehiculoId = vehiculoId; }
