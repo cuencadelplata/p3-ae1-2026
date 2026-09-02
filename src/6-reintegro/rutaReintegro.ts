c016563f4cef9eb086fdf1089de3322b09259f9e
Aylen <aylenagostina2006@gmail.com>
2026-09-02 10:24:06 +0000

    validacion de reintegro y openapi

--- src/6-reintegro/rutaReintegro.ts
+++ src/6-reintegro/rutaReintegro.ts
@@ -4,8 +4,16 @@ import { calculoReintegro } from "./calculoReintegro";
 const router = Router();
 
 router.post("/reintegro", (req, res) => {
-  const { montoCancelacion, viajeId } = req.body; //recibe el monto y el id del viaje
-  const monto = calculoReintegro(montoCancelacion); //monto calculado para reintegrar
+  const { montoCancelacion, viajeId } = req.body; // recibe el monto y el id del viaje
+
+  // validación: si falta algún dato, o montoCancelacion no es un número para y devulve un error
+  if (typeof montoCancelacion !== "number" || !viajeId) {
+    return res.status(400).json({
+      error: "montoCancelacion y viajeId son requeridos"
+    });
+  }
+
+  const monto = calculoReintegro(montoCancelacion); // monto calculado para reintegrar
 
   res.json({ monto, viajeId });
 });
