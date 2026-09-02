62d03875212a09edc8ca08bbbe5c7385404c2cfc
aylen <aylenchiesaok@gmail.com>
2026-09-02 01:09:55 -0300

    apperror

--- m3-vehiculos/src/errors/conflictError.ts
+++
@@ -1,8 +0,0 @@
-// Error de conflicto: el dato que se quiere crear ya existe
-// (ej: una patente duplicada). Se traduce a status 409.
-export class ConflictError extends Error {
-  constructor(message: string) {
-    super(message);
-    this.name = "ConflictError";
-  }
-}
