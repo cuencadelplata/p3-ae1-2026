50897bc8a83fd6953cf86fd9fb1eebc34e92e46e
lau184 <agostinacod@gmail.com>
2026-09-24 22:39:28 -0300

    avanace de bd

---
+++ m6_viajes/db/init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS viajes (
+    id VARCHAR(50) PRIMARY KEY,
+    cliente_id VARCHAR(50) NOT NULL,
+    conductor_id VARCHAR(50),
+    estado VARCHAR(30) NOT NULL,
+    origen TEXT NOT NULL,
+    destino TEXT NOT NULL,
+    codigo_verificacion VARCHAR(20) NOT NULL,
+    fecha_creacion TIMESTAMP NOT NULL DEFAULT now()
+);
